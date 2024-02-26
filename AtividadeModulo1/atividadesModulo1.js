d5344a926f4d91af528040a0ba525fba7c8f5dbc
btwSanToS <andrelmsantos.87@gmail.com>
2024-02-26 14:13:30 -0300

    Atividade 2

--- AtividadeModulo1/atividadesModulo1.js
+++ AtividadeModulo1/atividadesModulo1.js
@@ -24,3 +24,19 @@ function cumprimentar(text) {
 
 const saudacao = cumprimentar('André')
 console.log(saudacao);
+
+/* 02)
+Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+OBS: Considere que um ano tem 365 dias. Desconsidere anos bissextos e desconsidere também dias decorridos desde o último aniversário.
+*/
+
+function converterIdadeDias(idadeEmAnos){
+    const diasPorAno = 365
+    const idadeEmDias = idadeEmAnos * diasPorAno
+
+    return idadeEmDias;
+
+}
+const idade = 80
+const idadeConvertida = converterIdadeDias(idade);
+console.log(`A idade de ${idade} anos é equivalente a ${idadeConvertida} dias.`);
