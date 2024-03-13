87e3bedc73569f6d446e2be4e6f02801536f552c
btwSanToS <andrelmsantos.87@gmail.com>
2024-03-12 22:30:11 -0300

    atividade 10

--- AtividadeModulo1/atividadesModulo1.js
+++ AtividadeModulo1/atividadesModulo1.js
@@ -181,3 +181,21 @@ function repeticaoParametro(quantidade) {
 }
 
 console.log(repeticaoParametro(6))
+
+/* 10)
+Crie uma função que receba uma array e retorne o primeiro e o último elemento desse array como um novo
+array: 
+receberPrimeiroEUltimoElemento([7,14,"olá"]) // retornará [7, "olá"] 
+receberPrimeiroEUltimoElemento([-100, "aplicativo", 16]) // retornará [-100, 16]
+*/
+
+function receberPrimeiroEUltimoElemento(array) {
+    if (array.length === 0) {
+        return [];
+    } else {
+        return [array[0], array[array.length - 1]];
+    }
+}
+
+console.log(receberPrimeiroEUltimoElemento([7, 14, "olá"]));
+console.log(receberPrimeiroEUltimoElemento([-100, "aplicativo", 16]));
