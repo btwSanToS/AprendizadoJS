5e3cbbb94eb808aa4b4e222017a58685626b3b43
btwSanToS <andrelmsantos.87@gmail.com>
2024-04-01 14:14:47 -0300

    atividade 15

--- AtividadeModulo1/atividadesModulo1.js
+++ AtividadeModulo1/atividadesModulo1.js
@@ -303,3 +303,22 @@ const receberSomenteOsParesDeIndicesPares = function (numeral) {
 }
 console.log(receberSomenteOsParesDeIndicesPares([1, 2, 3, 4]));
 console.log(receberSomenteOsParesDeIndicesPares([10, 70, 22, 43]));
+
+/* 15)
+A fim de manter o calendário anual ajustado com o movimento de translação da Terra, criou-se os anos bissextos, que têm 366 dias em vez dos 365 presentes nos anos normais.
+Para determinar se um ano é bissexto, é necessário saber se ele é multiplo de 4. Não pode ser múltiplo de 100, exceto se for também múltiplo de 400.
+Com isso em mente, desenvolva uma função que recebe um número correspondente a um ano e retorna se ele é bissexto ou não.
+
+checarAnoBissexto(2020) // retornará true
+checarAnoBissexto(2100) // retornará false, pois é múltiplo de 100 e não é múltiplo de 400
+*/
+
+const checarAnoBissexto = function(anoReferencia) {
+    if ((anoReferencia % 4 === 0 && anoReferencia % 100 !== 0) || anoReferencia % 400 === 0){
+        return true
+    } else {
+        return false
+    }
+}
+console.log(checarAnoBissexto(2020))
+console.log(checarAnoBissexto(2100))
