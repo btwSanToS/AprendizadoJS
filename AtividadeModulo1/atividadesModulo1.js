f69a77aff6ee654696673d5cabc46d7e1936c94a
btwSanToS <andrelmsantos.87@gmail.com>
2024-04-15 14:32:25 -0300

    atividade 25

--- AtividadeModulo1/atividadesModulo1.js
+++ AtividadeModulo1/atividadesModulo1.js
@@ -471,3 +471,24 @@ const buscarPalavrasSemelhantes = function (partePalavra, palavra) {
 }
 console.log(buscarPalavrasSemelhantes("pro", ["programação", "mobile", "profissional"]))
 console.log(buscarPalavrasSemelhantes("python", ["javascript", "java", "c++"]))
+
+/* 25)
+Desenvolva uma função que receba uma string como parâmetro e retorne essa string somente com as consoantes, ou seja, sem as vogais.
+
+removerVogais("Cod3r") // retornará "Cd3r" 
+removerVogais("Fundamentos") // retornará "Fndmnts"
+*/
+
+const removerVogais = function (palavra) {
+    let consoantes = ""
+    for (runner = 0; runner < palavra.length; runner++) {
+        caracter = palavra[runner]
+        if (!"aeiouAEIOU".includes(caracter)) {
+            consoantes += caracter
+        }
+    }
+    return consoantes
+}
+
+console.log(removerVogais("Cod3r"))
+console.log(removerVogais("Fundamentos"))
